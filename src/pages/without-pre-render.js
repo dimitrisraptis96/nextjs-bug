de45ac74afa1f2d4e3186b94b90be64c43968600
Jim Raptis <dimitrisraptis96@gmail.com>
2023-05-29 18:59:39 +0300

    iconoir import

--- src/pages/without-pre-render.js
+++ src/pages/without-pre-render.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { Center, Text } from "@chakra-ui/react";
+import { EyeClose } from "iconoir-react";
 
 export default function Page({ templates = [] }) {
   return (
     <Center w="100vw" h="100vh">
+      <EyeClose />
       <Text>😢Without pre-render</Text>
     </Center>
   );
