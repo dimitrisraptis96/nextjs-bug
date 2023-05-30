c3719431cf8c3255d8b7fa40157e7c6f2527620c
Jim Raptis <dimitrisraptis96@gmail.com>
2023-05-30 10:26:51 +0300

    remove component, use import though

--- src/pages/without-pre-render.js
+++ src/pages/without-pre-render.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Center, Text } from "@chakra-ui/react";
 import { EyeClose } from "iconoir-react";
+import { motion } from "framer-motion";
 
 export default function Page({ templates = [] }) {
   return (
     <Center w="100vw" h="100vh">
-      <EyeClose />
+      {/* <EyeClose /> */}
       <Text>😢Without pre-render</Text>
     </Center>
   );
